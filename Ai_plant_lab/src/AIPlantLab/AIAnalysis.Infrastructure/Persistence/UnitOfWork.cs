a90d262ca7253f2cf8c86dd24293a0f1e5477673
bozhena_diakiv <bozhena.diakiv@syngenta.com>
2026-07-16 14:58:20 +0300

    fix unit of work to not use new for repository creation

--- Ai_plant_lab/src/AIPlantLab/AIAnalysis.Infrastructure/Persistence/UnitOfWork.cs
+++ Ai_plant_lab/src/AIPlantLab/AIAnalysis.Infrastructure/Persistence/UnitOfWork.cs
@@ -1,5 +1,4 @@
 using AIAnalysis.Application.Interfaces.Repositories;
-using AIAnalysis.Infrastructure.Persistence.Repositories;
 
 namespace AIAnalysis.Infrastructure.Persistence;
 
@@ -10,11 +9,14 @@ public sealed class UnitOfWork : IUnitOfWork
     public IPlantDiagnosisRepository Diagnoses { get; }
     public IDiseaseRepository Diseases { get; }
 
-    public UnitOfWork(AppDbContext dbContext)
+    public UnitOfWork(
+        AppDbContext dbContext,
+        IPlantDiagnosisRepository diagnoses,
+        IDiseaseRepository diseases)
     {
         _dbContext = dbContext;
-        Diagnoses = new PlantDiagnosisRepository(_dbContext);
-        Diseases = new DiseaseRepository(_dbContext);
+        Diagnoses = diagnoses;
+        Diseases = diseases;
     }
 
     public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
